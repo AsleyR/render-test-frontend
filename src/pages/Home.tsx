81f65f5e6539fbe0dc0907cd734762fb49d1827f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-30 16:29:25 -0600

    Update 'Home' page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,17 +7,6 @@ const Home = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Frontend test example of a React App.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React?
-        </a>
         <Message/>
       </header>
     </div>
